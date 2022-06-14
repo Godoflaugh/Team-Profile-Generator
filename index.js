6f1872d845316f3d61702b7ca26a91eaa32b97b5
Godoflaugh <lam.kevin91@gmail.com>
2022-06-14 15:48:50 -0700

    Completed final testing, and html/css corrections for the markup

--- index.js
+++ index.js
@@ -19,14 +19,93 @@ const Manager = require('./lib/Manager')
 
 
 //TODO:Create HTML template to be used, the output will filter to the dist folder
-const generateHTML = ({ name, id, email, office }) =>
+const generateHTML = () => {
+  return `<!DOCTYPE html>
+<html>
+<title>Team Profile Generator</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
 
-  //Starting back tick
+
+  <h2 class="w3-center w3-pink w3-center">Team Profile Generator</h2>
   `
+}
 
+const generateManager = (manager) => {
+  return `
+<div class="w3-container w3-center">
+  <div class="w3-card-4 w3-centered" style="width:100%;">
+    <header class="w3-container w3-blue w3-center">
+      <h1>${manager.name}</h1>
+    </header>
 
-`
-// Ending backtick ^
+    <div class="w3-container w3-center">
+      <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+      <p>ID: ${manager.id}</p>
+      <p>Role: ${manager.role}<p>
+      <p>Office Number: ${manager.officeNumber}
+    </div>
+
+    <footer class="w3-container w3-blue w3-center">
+      <h5>Boss Man</h5>
+    </footer>
+    </div>
+  </div>
+  <hr>`
+}
+
+const generateEngineer = (engineer) => {
+  return `
+  <div class="w3-container w3-center">
+    <div class="w3-card-4 w3-center" style="width:100%;">
+      <header class="w3-container w3-green">
+        <h1>${engineer.name}</h1>
+      </header>
+
+    <div class="w3-container w3-center">
+      <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+      <p>ID: ${engineer.id}</p>
+      <p>Role: ${engineer.role}<p>
+      <p>GitHub: <a href="https://github.com/${engineer.github}" target='_blank'>${engineer.github}</a></p>
+    </div>
+
+    <footer class="w3-container w3-green">
+      <h5>Great teachers!</h5>
+    </footer>
+    </div>
+  </div>
+  <hr>`
+}
+
+const generateIntern = (intern) => {
+  return `
+  <div class="w3-container w3-center">
+    <div class="w3-card-4 w3-center" style="width:100%;">
+      <header class="w3-container w3-red">
+        <h1>${intern.name}</h1>
+      </header>
+
+    <div class="w3-container w3-center">
+      <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+      <p>ID: ${intern.id}</p>
+      <p>Role: ${intern.role}<p>
+      <p>School: ${intern.school}</p>
+    </div>
+
+    <footer class="w3-container w3-red">
+      <h5>Great to have you on board!</h5>
+    </footer>
+  </div>
+</div>
+<hr>`
+}
+
+const generatebottomHTML = () => {
+  return `
+  </body>
+  </html>`
+}
 
 
 const managers = []
@@ -40,7 +119,7 @@ const newQuestion = () => {
     type: "list",
     name: 'role',
     message: "New Employee's role?",
-    chices: ['Manager, Engineer, Intern']
+    choices: ['Manager', 'Engineer', 'Intern', 'Finish']
   })
     .then((data) => {
       const answer = data
@@ -50,11 +129,21 @@ const newQuestion = () => {
         engineerQuestions()
       } else if (answer.role === 'Intern') {
         internQuestions()
-      } else {
-        console.log(managers, interns, engineers)
+      } else if (answer.role === 'Finish') {
+        module.exports = managers
+        module.exports = engineers
+        module.exports = interns
+        clearHTML()
+        topHTML()
+        managerGenerator()
+        engingeerGenerator()
+        internGenerator()
+        bottomHTML()
+        return answer
       }
     })
 }
+newQuestion()
 
 const managerQuestions = () => {
   return inquirer.prompt([
@@ -88,12 +177,13 @@ const managerQuestions = () => {
   ])
     .then((data) => {
       const newManager = new Manager(
-        data.name
-      data.id
-      data.email
-      data.officeNumber
+        data.name,
+        data.id,
+        data.email,
+        data.officeNumber,
       )
       managers.push(newManager)
+      console.log(data)
       newQuestion()
     })
 }
@@ -127,12 +217,13 @@ const engineerQuestions = () => {
   ])
     .then((data) => {
       const newEngineer = new Engineer(
-        data.name
-      data.id
-      data.email
-      data.github
+        data.name,
+        data.id,
+        data.email,
+        data.github,
       )
       engineers.push(newEngineer)
+      console.log(data)
       newQuestion()
     })
 }
@@ -163,23 +254,44 @@ internQuestions = () => {
   ])
     .then((data) => {
       const newIntern = new Intern(
-        data.name
-      data.id
-      data.email
-      data.school
+        data.name,
+        data.id,
+        data.email,
+        data.school,
       )
       interns.push(newIntern)
+      console.log(data)
       newQuestion()
     })
 }
 
+const clearHTML = () => {
+  fs.unlinkSync('./index.html')
+}
 
+// Html Markdown generation
+const topHTML = () => {
+  fs.appendFileSync('index.html', generateHTML())
+}
 
+const managerGenerator = () => {
+  managers.forEach(manager => {
+    fs.appendFileSync('index.html', generateManager(manager))
+  })
+}
 
+const engingeerGenerator = () => {
+  engineers.forEach(engineer => {
+    fs.appendFileSync('index.html', generateEngineer(engineer))
+  })
+}
 
+const internGenerator = () => {
+  interns.forEach(intern => {
+    fs.appendFileSync('index.html', generateIntern(intern))
+  })
+}
 
-
-
-
-
-questions()
+const bottomHTML = () => {
+  fs.appendFileSync('index.html', generatebottomHTML())
+}
